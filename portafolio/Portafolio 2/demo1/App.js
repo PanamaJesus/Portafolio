aed930ba1742370e3d3df6e7782f1b48d89c9ef9
Panama_Are_Jesus <140968629+PanamaJesus@users.noreply.github.com>
2024-03-14 22:55:56 -0700

    Add files via upload

---
+++ portafolio/Portafolio 2/demo1/App.js
@@ -0,0 +1,100 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import * as ImagePicker from "expo-image-picker";
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button'
+import CircleButton from './components/CircleButton';
+import Iconbutton from './components/Iconbutton';
+import EmojiPicker from './components/EmojiPicker';
+const PlaceholderImage=require("./assets/images/favicon.png")
+
+export default function App() {
+  const [isModalVisible,setIsModalVisible]=useState(false);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const pickImageAsync =  async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        quality: 1,
+    })
+    if (!result.canceled) {
+        console.log(result)
+        setSelectedImage(result.assets[0].uri);
+        setShowAppOptions(true);
+    }else{
+        alert("You did not select any image")
+    }
+  }
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  }
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  }
+
+  const onSaveImageAsync = async () => {
+    
+  }
+
+  const onModalClose = ()=>{
+    setIsModalVisible=(false);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.image_container}>
+        <ImageViewer selectedImage={selectedImage} PlaceholderImageSource={PlaceholderImage} />
+      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <Iconbutton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <Iconbutton icon="save-alt" label='Save' onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ): (<View style={styles.footer_container}>
+        <Button onPress={pickImageAsync} theme = "primary" label="Chose a Photo" />
+        <Button onPress={() => setShowAppOptions(true)} label="Use this Photo" />
+      </View>
+      )}
+      {/* <Text style={styles.text_props}>Open up App.js to start working on your app!</Text>
+      <Text style={{color: "#FFFFFF"}}>Hola Mundo React Native</Text> */}
+      <EmojiPicker isVisible={isModalVisible} onclose={onModalClose}>
+
+      </EmojiPicker>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2E4056',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text_props: {
+    color: "#FFFFFF",
+  },
+  image_container: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footer_container: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+});
